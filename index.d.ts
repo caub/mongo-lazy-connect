bb5bdd5780dcad8ec92bd2773b251ea3bd9d7527
Andrew <5755685+arnidan@users.noreply.github.com>
2019-10-17 16:42:47 +0300

    Fix types for typescript

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-import { MongoClient, MongoClientOptionsn, Db } from "@types/mongodb";
+import { MongoClient, MongoClientOptions, Db } from "mongodb";
 
-export default function (uri: string, options: MongoClientOptions): Promise<MongoClient | Db>;
+export default function (uri: string, options: MongoClientOptions): Db;
